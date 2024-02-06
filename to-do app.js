05be95bb9e64e435acb37e8916ca4fcc5054bbb0
DivyaLalithaT <158284417+DivyaLalithaT@users.noreply.github.com>
2024-02-06 17:23:54 +0530

    to-do application

---
+++ to-do app.js
@@ -0,0 +1,42 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+    let errorMessage = document.getElementById("errorMessage");
+
+    if (taskInput.value !== "") {
+        errorMessage.style.display = "none";
+
+        let listItem = document.createElement("li");
+
+        let deleteIcon = document.createElement("span");
+        deleteIcon.classList.add("delete-icon");
+        deleteIcon.innerHTML = "&#10006;"; 
+        deleteIcon.onclick = function () {
+            taskList.removeChild(listItem);
+        };
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.addEventListener("change", function () {
+            if (checkbox.checked) {
+                listItem.style.textDecoration = "line-through";
+                deleteIcon.style.textDecoration = "none"; // Prevent delete icon from receiving line-through
+            } else {
+                listItem.style.textDecoration = "none";
+            }
+        });
+    
+        let label = document.createElement("label");
+        label.appendChild(checkbox);
+        label.appendChild(document.createTextNode(taskInput.value));
+
+        listItem.appendChild(label);
+        listItem.appendChild(deleteIcon);
+
+        taskList.appendChild(listItem);
+
+        taskInput.value = "";
+    } else {
+        errorMessage.style.display = "block";
+    }
+}
